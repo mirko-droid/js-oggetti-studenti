0cc5f9260818eff4c38a56f15bdb32c74ec56b83
Mirko <mirko.saponaro91@gmail.com>
2020-12-03 18:17:55 +0100

    Creato array di oggetti
    Creato array di oggetti per utilizzarli come array

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,7 @@ $(function() {
         cognome : 'franco',  // se abbiamo più proprietà le divido con una [ , ] . 
         età : 23
     }; 
+    console.log(studente);
 // 2. Stampare a schermo attraverso il for in tutte le proprietà.
 var risultatoCard = ' '; // inizializzo la variabile con una stringa per ottenere nel risultato output tutte le proprietà e i valori, semplicemente concatendando in html qesta variabile e gli altri elementi che desideriamo vedere come stringa in html attraverso l'operatore ( + )  e il for in
 
@@ -31,9 +32,37 @@ for (var key in studente) {
 //Stampare a schermo
 $('#elenco_proprietà').html(risultatoCard);
 
+// 3. Creare un array di oggetti di studenti. 
 
+var studenti = [
+    {
+        name : 'Antonio',
+        cognome : 'Sartori',
+        età : 23
+    },
+    {
+        name : 'Paolo',
+        cognome : 'Bianchi',
+        età : 23
+    },
+    {
+        name : 'Giacomo',
+        cognome : 'Rossi',
+        età : 23
+    }
+];
+console.log(studenti);
 
+// Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+for (var i = 0; i < studenti.length; i++) {
+    //trasformo gli oggetti dell'array in array così da poterli usare con gli strumenti degli array
+    var myObjectsArrayTranform = Object.values(studenti[i]);
+    //ciclo all'interno dell'array creato il mio blocco di codice per mostrare a video(o stampare in console) i primi due elementi dell'array (nome e cognome)
+    for (var i = 0; i < myObjectsArrayTranform.length -1; i++) {
+        console.log(myObjectsArrayTranform[0] + ' ' + myObjectsArrayTranform[1]);
+    }
 
+}
 
 
 
